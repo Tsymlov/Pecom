e0ff4d58e9f7f232413527e1463bbd640b0f7830
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-01-25 14:39:11 +0300

    Initial Commit

---
+++ Pecom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pecom
+//
+//  Created by Alexey Tsymlov on 1/25/15.
+//  Copyright (c) 2015 Alexey Tsymlov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pecom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pecom
+//
+//  Created by Alexey Tsymlov on 1/25/15.
+//  Copyright (c) 2015 Alexey Tsymlov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
