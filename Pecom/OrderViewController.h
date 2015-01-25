6f1318015be845c166b5243a3a375ab61de365ce
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-01-25 16:42:36 +0300

    OrderViewController was added.

---
+++ Pecom/OrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrderViewController.h
+//  Pecom
+//
+//  Created by Alexey Tsymlov on 1/25/15.
+//  Copyright (c) 2015 Alexey Tsymlov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderViewController : UIViewController
+
+@end
